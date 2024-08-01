65c3ef28f034785de8306158eba7485de444b860
adrian peralta <eadrian.peralta@gmail.com>
2024-08-01 19:46:30 +0000

    config: #1 vue scaffold

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -1,12 +1,12 @@
 // const { defineConfig } = require('@playwright/test');
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './e2e',
-  outputDir: './e2e/test-results/',
+  testDir: "./e2e",
+  outputDir: "./e2e/test-results/",
   timeout: 30000,
   expect: {
-    timeout: 5000
+    timeout: 5000,
   },
   // reporter: [
   //   ['list'],
