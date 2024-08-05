63c4717ecde7009e6e92efc633a211a303b294cb
adrian peralta <eadrian.peralta@gmail.com>
2024-08-05 15:24:00 +0000

    feat: #2 define api and mocks for weeks dropdown

--- frontend/src/api/CompanyServiceApi.ts
+++ frontend/src/api/CompanyServiceApi.ts
@@ -1,5 +1,7 @@
 import CompanyServices from "@/mock/company_services.json";
-import { CompanyService } from "@/api/types";
+import WeeksServiceA from "@/mock/weeks_service_a.json";
+import WeeksServiceB from "@/mock/weeks_service_b.json";
+import { CompanyService, Weeks } from "@/api/types";
 
 const isMock = process.env.VUE_APP_USE_MOCK === "true";
 
@@ -20,8 +22,35 @@ export const fetchCompanyServices = async (): Promise<CompanyService[]> => {
       return data;
     } catch (error) {
       console.error(error);
-      console.error("Failed to connect to the backend");
-      throw new Error("Failed to connect to the backend");
+      console.error("Failed to fetch Company Services");
+      throw new Error("Failed to fetch Company Services");
+    }
+  }
+};
+
+export const requestWeeks = async (serviceId: number): Promise<Weeks> => {
+  if (isMock) {
+    return new Promise<Weeks>((resolve) => {
+      setTimeout(() => {
+        if (serviceId === 1) {
+          resolve(WeeksServiceA.data);
+        } else {
+          resolve(WeeksServiceB.data);
+        }
+      }, 500);
+    });
+  } else {
+    try {
+      const response = await fetch(`/api/company_services/${serviceId}/weeks`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const { data } = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      console.error("Failed to fetch weeks");
+      throw new Error("Failed to fetch weeks");
     }
   }
 };
--- frontend/src/api/types.ts
+++ frontend/src/api/types.ts
@@ -6,3 +6,22 @@ export interface CompanyService {
   id: number;
   name: string;
 }
+
+export interface Week {
+  week: string;
+  start_date: string;
+  end_date: string;
+}
+export interface Weeks {
+  past: Week[];
+  future: Week[];
+}
+
+export interface WeeksResponse {
+  data: {
+    past: Week[];
+    future: Week[];
+  };
+  status: number;
+  statusText: string;
+}
