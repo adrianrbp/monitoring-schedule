f0f4b7811eede00e871bed7b84314d850e9bf5d9
adrian peralta <eadrian.peralta@gmail.com>
2024-08-08 05:05:13 +0000

    refactor: #6 move Json response related logic from application controller to concern

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,22 +1,3 @@
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
-  rescue_from ActionController::RoutingError, with: :render_not_found
-
-  before_action :ensure_json_request
-  before_action :set_default_format
-
-  def render_not_found
-    render json: { error: 'Not Found' }, status: :not_found
-  end
-
-  private
-  def set_default_format
-    request.format = :json
-  end
-
-  def ensure_json_request
-    return if request.format.json?
-    render json: { error: 'Not Acceptable' }, status: :not_acceptable
-  end
-
+  include JsonResponse
 end
---
+++ backend/app/controllers/concerns/json_response.rb
@@ -0,0 +1,25 @@
+module JsonResponse
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from ActionController::RoutingError, with: :render_not_found
+
+    before_action :ensure_json_request
+    before_action :set_default_format
+  end
+
+  def render_not_found
+    render json: { error: 'Not Found' }, status: :not_found
+  end
+
+  private
+  def set_default_format
+    request.format = :json
+  end
+
+  def ensure_json_request
+    return if request.format.json?
+    render json: { error: 'Not Acceptable' }, status: :not_acceptable
+  end
+end
