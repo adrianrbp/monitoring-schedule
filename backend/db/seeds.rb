0f6020396db101e89bb20d857adc8bfaf0104dec
adrian peralta <eadrian.peralta@gmail.com>
2024-08-09 20:55:46 +0000

    feat: #6 update seeds to create CompanyServices with Engineers

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,10 +1,20 @@
 require 'faker'
 
+CompanyServiceEngineer.destroy_all
 CompanyService.destroy_all
 Engineer.destroy_all
 
-10.times { FactoryBot.create(:company_service) }
-puts "10 Company Services Created"
+3.times do
+  company_service = FactoryBot.create(:company_service)
 
-10.times { FactoryBot.create(:engineer) }
-puts "10 Engineers Created"
+  # Create engineers and associate them with the company service
+  engineers = FactoryBot.create_list(:engineer, 3) # Creates 3 engineers
+  engineers.each do |engineer|
+    CompanyServiceEngineer.create!(
+      company_service: company_service,
+      engineer: engineer
+    )
+  end
+end
+puts "3 Company Services Created"
+puts "9 Engineers Created"
