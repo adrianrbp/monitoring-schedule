ecfd34438624d7243eb6e879cec31cd0cc129c18
adrian peralta <eadrian.peralta@gmail.com>
2024-08-09 20:45:46 +0000

    feat: #6 limit 3 engineers per company service

--- backend/app/models/company_service_engineer.rb
+++ backend/app/models/company_service_engineer.rb
@@ -1,4 +1,14 @@
 class CompanyServiceEngineer < ApplicationRecord
   belongs_to :company_service
   belongs_to :engineer
+
+  validate :limited_engineers
+
+  private
+
+  def limited_engineers
+    if company_service.present? && company_service.company_service_engineers.count >= 3
+      errors.add(:base, "Cannot assign more than 3 engineers to a company service")
+    end
+  end
 end
