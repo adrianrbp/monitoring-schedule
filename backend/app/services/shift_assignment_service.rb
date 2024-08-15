0b2f56dd2b7c3dd3d06558e798dea9455e3cab20
adrian peralta <eadrian.peralta@gmail.com>
2024-08-15 15:26:39 +0000

    feat: #10 add phase 2 algorithm: assign shifts where there are 2 or more engineers available

--- backend/app/services/shift_assignment_service.rb
+++ backend/app/services/shift_assignment_service.rb
@@ -11,9 +11,14 @@ class ShiftAssignmentService
   def assign_shifts
     shifts = Shift.where(company_service: company_service, week: week)
 
-    # Fase 1: Asignación inicial en horarios donde solo un ingeniero está disponible
+    # Fase 1: Asignación inicial - horarios donde solo un ingeniero está disponible
     shifts.each do |shift|
       assign_shifts_with_unique_availability(shift)
+    end
+
+    # Fase 2: Asignación optimizada - horarios con múltiples ingenieros disponibles
+    shifts.each do |shift|
+      assign_shifts_with_collision(shift, 2)
     end
 
     save_assignments(assignments)
@@ -21,20 +26,15 @@ class ShiftAssignmentService
 
   private
 
-
   def load_availabilities
     Availability.where(week: week)
                 .includes(:engineer)
                 .group_by { |a| [a.day, a.time] }
   end
 
-
   def assign_shifts_with_unique_availability(shift)
     (shift.start_hour...shift.end_hour).each do |hour|
       available_engineers = availabilities[[shift.day, hour]].map(&:engineer)
-      puts "#{shift.day}#{hour}"
-      puts "eng: #{available_engineers.size} -> #{available_engineers.first.name}"
-      puts availabilities.count
 
       if available_engineers.size == 1
         engineer = available_engineers.first
@@ -56,6 +56,56 @@ class ShiftAssignmentService
     end
   end
 
+  def assign_shifts_with_collision(shift, collision_count)
+    minimum_consecutive_hours = 4
+    shift_hours = (shift.start_hour...shift.end_hour).to_a
+
+    shift_hours.each_with_index do |hour, index|
+      next if assignments.any? { |a| a[:shift] == shift && a[:start_hour] == hour }
+
+      available_engineers = availabilities[[shift.day, hour]].map(&:engineer)
+      # Filter cases where availability in an hour is 2 or 3 (collision_count)
+      if available_engineers.size == collision_count
+        sorted_engineers = available_engineers.sort_by { |e| total_hours_assigned(e) }
+
+        sorted_engineers.each do |engineer|
+          block_start = hour
+          block_end = block_start
+
+          # Intentar extender el bloque de horas hasta donde sea posible, con un mínimo de 4 horas
+          while block_end + 1 < shift.end_hour &&
+                availabilities[[shift.day, block_end + 1]].map(&:engineer).include?(engineer)
+            block_end += 1
+          end
+
+          # Verificar si el bloque tiene al menos 4 horas consecutivas
+          if block_end - block_start + 1 >= minimum_consecutive_hours
+            # Crear asignación para el bloque de horas consecutivas
+            assignments << {
+              engineer: engineer,
+              shift: shift,
+              start_hour: block_start,
+              end_hour: block_end + 1
+            }
+
+            # Saltar las horas ya asignadas en el bloque
+            shift_hours.slice!(index, block_end - block_start + 1)
+            break
+          end
+        end
+
+        # Assign 1 hour if no continuous hours
+        unless shift_hours.empty?
+          assignments << {
+            engineer: sorted_engineers.first,
+            shift: shift,
+            start_hour: hour,
+            end_hour: hour + 1
+          }
+        end
+      end
+    end
+  end
 
   def save_assignments(assignments)
     EngineerShift.transaction do
@@ -70,4 +120,8 @@ class ShiftAssignmentService
     end
   end
 
+  def total_hours_assigned(engineer)
+    assignments.select { |a| a[:engineer] == engineer }
+               .sum { |a| a[:end_hour] - a[:start_hour] }
+  end
 end
--- backend/spec/services/shift_assignment_service_spec.rb
+++ backend/spec/services/shift_assignment_service_spec.rb
@@ -34,6 +34,18 @@ RSpec.describe ShiftAssignmentService, type: :service do
                               "Wednesday" => (19..23).to_a, # 2 Engineers
                             }) }
   # Total Availabilities: 35
+  # Ideal assignments:
+  # - Monday:    Engineer 1 | horas disponibles: (19..23) -> 5h
+  # - Tuesday:   Engineer 1,2,3 | horas disponibles: (19..23)
+  # - Wednesday: Engineer 2,3 | horas disponibles: (19..23)
+  # - Thursday:  Engineer 2 | horas disponibles: (19..23) -> 5h
+  # Ammount:
+  # Engineer 1: 5h +
+  # Engineer 2: 5h +
+  # Engineer 3:
+
+  # let!(:monday_shift) { create(:shift, company_service: company_service, week: week, day: 'Monday', start_hour: 19, end_hour: 23) }
+  # let!(:tuesday_shift) { create(:shift, company_service: company_service, week: week, day: 'Tuesday', start_hour: 19, end_hour: 23) }
 
   let!(:shifts) do
     %w[Monday Tuesday Wednesday Thursday].map do |day|
@@ -79,6 +91,62 @@ RSpec.describe ShiftAssignmentService, type: :service do
       expect(assignments[0][:engineer]).to eq(engineer2)
       expect(assignments[0][:start_hour]).to eq(19)
       expect(assignments[0][:end_hour]).to eq(23)
+    end
+  end
+
+  describe '#assign_shifts_with_multiple_availability' do
+    before do
+      @service = ShiftAssignmentService.new(company_service, week)
+      shifts.each do |shift|
+        @service.send(:assign_shifts_with_unique_availability, shift)
+      end
+    end
+
+    it 'Wednesday: Assign Engineer 3 (less hours assigned) - collision of 2 engineers' do
+
+      wednesday_shift = shifts[2]
+      @service.send(:assign_shifts_with_collision, wednesday_shift, 2)
+
+      assignments = @service.assignments
+
+      expect(assignments.count).to eq(3) # monday, thursday and wednesday
+      expect(assignments[0][:engineer]).to eq(engineer1)
+      expect(assignments[0][:start_hour]).to eq(19)
+      expect(assignments[0][:end_hour]).to eq(23)
+
+      expect(assignments[1][:engineer]).to eq(engineer2)
+      expect(assignments[1][:start_hour]).to eq(19)
+      expect(assignments[1][:end_hour]).to eq(23)
+
+      expect(assignments[2][:engineer]).to eq(engineer3)
+      expect(assignments[2][:start_hour]).to eq(19)
+      expect(assignments[2][:end_hour]).to eq(23)
+    end
+    it 'Tuesday: Assign Engineer 1 (less hours assigned) - collision of 3 engineers' do
+      # IMPROVE: Random assignment and store over weeks
+      wednesday_shift = shifts[2]
+      @service.send(:assign_shifts_with_collision, wednesday_shift, 2)
+      tuesday_shift = shifts[1]
+      @service.send(:assign_shifts_with_collision, tuesday_shift, 3)
+
+      assignments = @service.assignments
+
+      expect(assignments.count).to eq(4) # monday, thursday, wednesday and tuesday
+      expect(assignments[0][:engineer]).to eq(engineer1)
+      expect(assignments[0][:start_hour]).to eq(19)
+      expect(assignments[0][:end_hour]).to eq(23)
+
+      expect(assignments[1][:engineer]).to eq(engineer2)
+      expect(assignments[1][:start_hour]).to eq(19)
+      expect(assignments[1][:end_hour]).to eq(23)
+
+      expect(assignments[2][:engineer]).to eq(engineer3)
+      expect(assignments[2][:start_hour]).to eq(19)
+      expect(assignments[2][:end_hour]).to eq(23)
+
+      expect(assignments[3][:engineer]).to eq(engineer1)
+      expect(assignments[3][:start_hour]).to eq(19)
+      expect(assignments[3][:end_hour]).to eq(23)
     end
   end
 
